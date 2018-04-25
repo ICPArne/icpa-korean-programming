e8782223eb0e715cf799b8ca7f9d2e90d7dbb85c
ICPArne <38715609+ICPArne@users.noreply.github.com>
2018-04-25 15:50:01 +0900

    JongHoon.py
    김종훈을 클래스화 시키고 있습니다

---
+++ JongHoon.py
@@ -0,0 +1,6 @@
+class JongHoon(object):
+    def __init__(self, name, age):
+        self.name = "JongHoon"
+        self.age = 18
+    def print_jonghoon(self, message):
+        print("Jonghoon said,", message)
