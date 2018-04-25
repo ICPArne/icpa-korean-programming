11851722d611b4dedd287d4a6bd600b74bb31fe5
ICPArne <38715609+ICPArne@users.noreply.github.com>
2018-04-25 15:53:27 +0900

    Update JongHoon.py

--- JongHoon.py
+++ JongHoon.py
@@ -1,6 +1,16 @@
 class JongHoon(object):
-    def __init__(self, name, age):
+    def __init__(self):
         self.name = "JongHoon"
         self.age = 18
     def print_jonghoon(self, message):
         print("Jonghoon said,", message)
+    def is_byeoungsin(self):
+        return True
+    
+jh = JongHoon()
+jh.print_jonghoon("빼애애액")
+
+##########################################
+##예상 출력 결과:##########################
+##빼애애액#################################
+##########################################
