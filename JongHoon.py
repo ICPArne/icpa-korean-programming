1705de44001a338dbadbb28032e29222f97122af
ICPArne <38715609+ICPArne@users.noreply.github.com>
2018-04-25 16:00:00 +0900

    김종훈 관련 클래스(테스트 중)
    여러가지가 수정되는 중인 걸 보는 중이에요

--- JongHoon.py
+++ JongHoon.py
@@ -10,8 +10,13 @@ class JongHoon(object):
     
 jh = JongHoon()
 jh.print_jonghoon("빼애애액")
-#
+if JongHoon.is_byeoungsin():
+    jh.print_jonghoon("으아앙")
+else:
+    jh.print_jonghoon("뿌애앵")
+
 ##########################################
 # 예상 출력 결과: #########################
-# 빼애애액 ################################
+# Jonghoon said, 빼애애액 #################
+# Jonghoon said, 으아앙 ###################
 ##########################################
