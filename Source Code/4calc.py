f54da4e3316ce8b4cd25e2ca8546149c76b9c26b
ICPA_RNE <38715609+ICPArne@users.noreply.github.com>
2018-05-30 15:10:04 +0900

    전세환의 코드 수정

--- Source Code/4calc.py
+++ Source Code/4calc.py
@@ -1,30 +1,61 @@
+# 최종 수정: 2018-05-30
+# 수정자: 20307 박진철
+
 import re
-def checkGobNa(a): #곱하기/나누기 연산
-    reRule=re.compile('([0-9.-]+)(곱하기|나누기)([0-9.-]+)')
-    searchResult=reRule.search(a)
-    if searchResult==None: #곱하기 또는 나누기가 더이상 없으면
-        return a           #결과 반환
-    if searchResult.group(2)=='곱하기': #곱하기일 때, 두 수 곱셈
-        calc=float(searchResult.group(1))*float(searchResult.group(3))
-    elif searchResult.group(2)=='나누기':
-        calc=float(searchResult.group(1))/float(searchResult.group(3))
-    result=a[:searchResult.span()[0]]+str(calc)+a[searchResult.span()[1]:] #나머지 내용+연산결과+나머지 내용
-    return checkGobNa(result) #재귀
 
-def checkPlusMinus(a):
-    reRule=re.compile('([0-9.-]+)(더하기|빼기)([0-9.-]+)')
-    searchResult=reRule.search(a)
-    if searchResult==None:
-        return a
-    if searchResult.group(2)=='더하기':
-        calc=float(searchResult.group(1))+float(searchResult.group(3))
-    elif searchResult.group(2)=='빼기':
-        calc=float(searchResult.group(1))-float(searchResult.group(3))
-    result=a[:searchResult.span()[0]]+str(calc)+a[searchResult.span()[1]:]
-    return checkPlusMinus(result)
+def checkMulDiv(text): #곱하기/나누기 연산
+    rule = re.compile('([0-9]+[.0-9]*[0-9]*)(곱하기|나누기)([0-9]+[,0-9]*[0-9]*)')
+    searchResult = rule.search(text)
 
-def doCalc(a):
-    return float(checkPlusMinus(checkGobNa(a))) #연산 순서
+    if searchResult == None:
+        return text.strip()
+    if searchResult.group(2) == '곱하기':
+        calc = float(searchResult.group(1)) * float(searchResult.group(3))
+    elif searchResult.group(2) == '나누기':
+        try:
+            calc = float(searchResult.group(1)) / float(searchResult.group(3))
+        except ZeroDivisionError:
+            print("0으로 나눌 수 없습니다.")
+            return
 
-asdf='1빼기100곱하기3나누기5곱하기2더하기10'
-print(doCalc(asdf))
+    result = text[:searchResult.span()[0]] + str(calc) + text[searchResult.span()[1]:]
+    return checkMulDiv(result)
+
+def checkPluMin(text): #더하기/빼기 연산
+    rule = re.compile('([0-9]+[.0-9]*[0-9]*)(더하기|빼기)([0-9]+[.0-9]*[0-9]*)')
+    searchResult = rule.search(text)
+
+    if searchResult == None:
+        return text.strip()
+    if searchResult.group(2) == '더하기':
+        calc = float(searchResult.group(1)) + float(searchResult.group(3))
+    elif searchResult.group(2) == '빼기':
+        calc = float(searchResult.group(1)) - float(searchResult.group(3))
+
+    result = text[:searchResult.span()[0]] + str(calc) + text[searchResult.span()[1]:]
+    return checkPluMin(result)
+
+def checkMath(text): #식 맞는지 확인 여부, 나중에 제대로 수정(ast 이용)
+    return True
+
+def doCalc(text):
+    text = text.replace(' ', '')
+    text = checkMulDiv(text)        #곱하기 계열 먼저 진행
+    if text == None:
+        return
+    text = checkPluMin(text)        #그 후 더하기 계열 진행
+    return float(text)
+
+#입력 형식: a 더하기|빼기|곱하기|나누기 b ... (띄어쓰기 O)        
+def main():
+    while True:
+        text = input("> ")
+        temp = doCalc(text)
+        if temp != None:
+            print(temp)
+        else:
+            pass
+        
+if __name__ == "__main__":
+    main()
+
