f49b46b7d7907405b109af3faef68143429712d5
ICPA_RNE <38715609+ICPArne@users.noreply.github.com>
2018-12-24 03:21:51 +0900

    배포된 패키지
    프로그래밍 언어 '고래' - 배포된 패키지에 해당합니다.

---
+++ Packaged/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'whale',
+    version          = '1.1.2',
+    description      = 'Korean programming language for elementary education',
+    author           = 'Jin Cheol Park',
+    author_email     = 'pjc05254@gmail.com',
+    url              = 'https://github.com/ICPArne/icpa-korean-programming',
+    install_requires = [],
+    packages         = ['whale'],
+    keywords         = ['Korean'],
+    python_requires  = '>=3',
+    zip_safe         = False
+)
