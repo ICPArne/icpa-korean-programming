393059fa60a2bb6aedf3baef1bc0bcb72ddc9d28
ICPA_RNE <38715609+ICPArne@users.noreply.github.com>
2018-12-24 12:04:36 +0900

    배포된 패키지
    배포된 패키지

--- Packaged/setup.py
+++ Packaged/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'whale',
-    version          = '1.1.2',
+    version          = '1.1.4',
     description      = 'Korean programming language for elementary education',
     author           = 'Jin Cheol Park',
     author_email     = 'pjc05254@gmail.com',
